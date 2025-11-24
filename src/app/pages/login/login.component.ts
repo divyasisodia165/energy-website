de51518e57b22ad7f9ebb74012d1f34a480578e7
Divya Sisodia <divyasisodia165@users.noreply.github.com>
2025-11-25 01:40:36 +0530

    Fixed login UI alignment and styling

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,  // IMPORTANT
+  imports: [CommonModule, FormsModule], // FIXES *ngFor, *ngIf, ngModel
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  
+  selectedCountry: string = 'United Kingdom';
+  selectedLocation: string = '';
+  showLoginForm: boolean = false;
+
+  countries: string[] = [
+    'United Kingdom',
+    'United States',
+    'Canada',
+    'Australia',
+    'India'
+  ];
+
+  locations: string[] = [
+    'London',
+    'Manchester',
+    'Birmingham',
+    'Leeds',
+    'Glasgow'
+  ];
+
+  onCountryChange(): void {
+    this.selectedLocation = '';
+    this.showLoginForm = false;
+  }
+
+  onLocationChange(): void {
+    if (this.selectedLocation) {
+      this.showLoginForm = true; // show email/password form
+    }
+  }
+
+  email: string = '';
+  password: string = '';
+
+}
