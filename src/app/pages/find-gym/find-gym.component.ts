036c55700ba479a8116433469c87e9647847219e
Divya Sisodia <divyasisodia165@users.noreply.github.com>
2026-03-12 19:10:37 +0530

    Updated gym scroll UI and map layout

--- src/app/pages/find-gym/find-gym.component.ts
+++ src/app/pages/find-gym/find-gym.component.ts
@@ -22,6 +22,10 @@ export class FindGymComponent implements OnInit {
   selectedTab: string = 'INDIA';
   selectedCountry: string = 'INDIA';
   mapUrl: SafeResourceUrl = '';
+
+  visibleGyms: Gym[] = [];
+itemsToShow = 10;
+currentIndex = 0;
   
   gymLocations = [
     { lat: 19.0760, lng: 72.8777, count: 35, address: 'Mumbai, India' },
@@ -34,22 +38,78 @@ export class FindGymComponent implements OnInit {
   ];
 
   worldwideGyms: Gym[] = [
-    { name: 'BALBRIGGAN GYM', location: 'Balbriggan, Ireland' },
-    { name: 'BALLINCOLLIG GYM', location: 'Ballincollig, Cork, Ireland' },
-    { name: 'CARLOW GYM', location: 'Carlow, Ireland' },
-    { name: 'CITYWEST GYM', location: 'Citywest, Dublin, Ireland' },
-    { name: 'CLAREHALL GYM', location: 'Clarehall, Dublin, Ireland' },
-    { name: 'DROGHEDA GYM', location: 'Drogheda, Louth, Ireland' },
-    { name: 'DUBLIN 8 GYM', location: 'Dublin 8, Ireland' },
-    { name: 'DUN LAOGHAIRE GYM', location: 'Dun Laoghaire, Dublin, Ireland' },
-    { name: 'DUNDALK GYM', location: 'Dundalk, Louth, Ireland' },
-    { name: 'GALWAY GYM', location: 'Galway, Ireland' },
-    { name: 'LIMERICK GYM', location: 'Limerick, Ireland' },
-    { name: 'MIDLETON GYM', location: 'Midleton, Cork, Ireland' },
-    { name: 'MULLINGAR GYM', location: 'Mullingar, Westmeath, Ireland' },
-    { name: 'SALTHILL GYM', location: 'Salthill, Galway, Ireland' },
-    { name: 'STEPASIDE GYM', location: 'Stepaside, Dublin, Ireland' }
-  ];
+{ name: 'ENERGIE FITNESS OLD STREET', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS HIGHBURY & ISLINGTON', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS ARCHWAY', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS KILBURN', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS CRICKLEWOOD', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS HOMERTON', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS BETHNAL GREEN', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS BOROUGH', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS BATTERSEA', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS PUTNEY', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS PALMERS GREEN', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS CHINGFORD', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS TOOTING', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS THORNTON HEATH', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS ACTON', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS ELTHAM', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS ERITH', location: 'London, UK' },
+{ name: 'ENERGIE FITNESS BRENTFORD', location: 'Brentford, UK' },
+{ name: 'ENERGIE FITNESS HAYES', location: 'Hayes, UK' },
+{ name: 'ENERGIE FITNESS HAMPTON HILL', location: 'Hampton Hill, UK' },
+{ name: 'ENERGIE FITNESS WOKINGHAM', location: 'Wokingham, UK' },
+{ name: 'ENERGIE FITNESS WINCHESTER', location: 'Winchester, UK' },
+{ name: 'ENERGIE FITNESS TUNBRIDGE WELLS', location: 'Kent, UK' },
+{ name: 'ENERGIE FITNESS CANTERBURY', location: 'Canterbury, UK' },
+{ name: 'ENERGIE FITNESS BOGNOR REGIS', location: 'Bognor Regis, UK' },
+{ name: 'ENERGIE FITNESS CRAWLEY', location: 'Crawley, UK' },
+{ name: 'ENERGIE FITNESS EAST GRINSTEAD', location: 'East Grinstead, UK' },
+{ name: 'ENERGIE FITNESS BRIGHTON', location: 'Brighton, UK' },
+{ name: 'ENERGIE FITNESS SANDHURST', location: 'Sandhurst, UK' },
+{ name: 'ENERGIE FITNESS ASHFORD', location: 'Surrey, UK' },
+{ name: 'ENERGIE FITNESS BEDFORD', location: 'Bedford, UK' },
+{ name: 'ENERGIE FITNESS IPSWICH', location: 'Ipswich, UK' },
+{ name: 'ENERGIE FITNESS CHELMSFORD', location: 'Chelmsford, UK' },
+{ name: 'ENERGIE FITNESS COLCHESTER', location: 'Colchester, UK' },
+{ name: 'ENERGIE FITNESS HODDESDON', location: 'Hoddesdon, UK' },
+{ name: 'ENERGIE FITNESS LETCHWORTH', location: 'Letchworth Garden City, UK' },
+{ name: 'ENERGIE FITNESS LEIGHTON BUZZARD', location: 'Leighton Buzzard, UK' },
+{ name: 'ENERGIE FITNESS LINCOLN CITY', location: 'Lincoln, UK' },
+{ name: 'ENERGIE FITNESS LINCOLN FLAVIAN RD', location: 'Lincoln, UK' },
+{ name: 'ENERGIE FITNESS KETTERING', location: 'Kettering, UK' },
+{ name: 'ENERGIE FITNESS HYDE', location: 'Stockport, UK' },
+{ name: 'ENERGIE FITNESS WARRINGTON', location: 'Warrington, UK' },
+{ name: 'ENERGIE FITNESS TUNSTALL', location: 'Stoke-on-Trent, UK' },
+{ name: 'ENERGIE FITNESS SCUNTHORPE', location: 'Scunthorpe, UK' },
+{ name: 'ENERGIE FITNESS WOLVERHAMPTON', location: 'Wolverhampton, UK' },
+{ name: 'ENERGIE FITNESS CHELMSLEY WOOD', location: 'Birmingham, UK' },
+{ name: 'ENERGIE FITNESS CHELTENHAM', location: 'Cheltenham, UK' },
+{ name: 'ENERGIE FITNESS BROOKLANDS', location: 'Milton Keynes, UK' },
+{ name: 'ENERGIE FITNESS INVERURIE', location: 'Inverurie, UK' },
+{ name: 'ENERGIE FITNESS CAMBUSLANG', location: 'Glasgow, UK' },
+{ name: 'ENERGIE FITNESS DUMBARTON', location: 'Dumbarton, UK' },
+{ name: 'ENERGIE FITNESS GLASGOW SOUTH', location: 'Glasgow, UK' },
+{ name: 'ENERGIE FITNESS CARDIFF', location: 'Cardiff, UK' },
+{ name: 'ENERGIE FITNESS BRIDGEND', location: 'Bridgend, UK' },
+{ name: 'ENERGIE FITNESS PONTYPRIDD', location: 'Pontypridd, UK' },
+{ name: 'ENERGIE FITNESS DEESIDE', location: 'Chester, UK' },
+{ name: 'ENERGIE FITNESS DRIMNAGH', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS DUN LAOGHAIRE', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS BALBRIGGAN', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS CITYWEST', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS CLAREHALL', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS DUBLIN 8', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS STEPASIDE', location: 'Dublin, Ireland' },
+{ name: 'ENERGIE FITNESS GALWAY', location: 'Galway, Ireland' },
+{ name: 'ENERGIE FITNESS LIMERICK', location: 'Limerick, Ireland' },
+{ name: 'ENERGIE FITNESS MULLINGAR', location: 'Mullingar, Ireland' },
+{ name: 'ENERGIE FITNESS DUNDALK', location: 'Dundalk, Ireland' },
+{ name: 'ENERGIE FITNESS DROGHEDA', location: 'Louth, Ireland' },
+{ name: 'ENERGIE FITNESS BALLINCOLLIG', location: 'Cork, Ireland' },
+{ name: 'ENERGIE FITNESS MIDLETON', location: 'Cork, Ireland' },
+{ name: 'ENERGIE FITNESS CARLOW', location: 'Carlow, Ireland' }
+];
 
   constructor(private sanitizer: DomSanitizer) {}
 
